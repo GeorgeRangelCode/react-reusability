c6ed26c5c1afc78dab036e2d404fc27be1539bc0
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2024-10-17 06:01:10 -0500

    Button - props review challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Button from "./Button";
+
+/**
+ * Challenge:
+ *
+ * Build a Button component in a separate file.
+ * (For styling purposes, make sure it renders an HTML <button> element)
+ * Your button component should take a `text` prop
+ */
+
 function App() {
-  return <h1>React App</h1>;
+  return (
+    <main>
+      <Button text="Click here" />
+    </main>
+  );
 }
 
 export default App;
