021af6e0a91b6dd00b2084c58b919184d435b9f7
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2024-11-15 06:26:56 -0500

    React.Children shortcomings

--- src/components/Menu/MenuDropdown.jsx
+++ src/components/Menu/MenuDropdown.jsx
@@ -1,3 +1,16 @@
-export default function MenuDropdown({ children, open }) {
-  return open ? <div className="menu-dropdown">{children}</div> : null;
+import React from "react";
+
+export default function MenuDropdown({ children, open, toggle }) {
+  /** Discovery Challenge:
+   * Make it so the children of MenuDropdown also have
+   * access to toggle and open.
+   */
+
+  return open ? (
+    <div className="menu-dropdown">
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, { toggle, open });
+      })}
+    </div>
+  ) : null;
 }
